66e08e123c6dd566c348cb1cc4bfe3bc9628032e
DionKelmendi <108992835+DionKelmendi@users.noreply.github.com>
2022-08-03 09:56:05 +0200

    Fixing some errors from last commit

---
+++ database/migrations/2022_08_03_073209_create_interviewees_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('interviewees', function (Blueprint $table) {
+            $table->id();
+            $table->String('name');
+            $table->String('surname');
+            $table->String('cv_path');
+            $table->String('external_cv_path');
+            $table->String('img');
+            $table->foreignId('interviewee_types_id')
+                ->references('id')->on('interviewee_types')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('interviewees');
+    }
+};
