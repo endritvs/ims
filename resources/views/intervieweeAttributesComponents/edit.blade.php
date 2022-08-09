342a1366de2a31794541214eaaf0929e4a1caaab
qendriimrexhepi <qendriimrexhepi@gmail.com>
2022-08-09 14:27:22 +0200

    all-interviewe-crud-modal

--- resources/views/intervieweeAttributesComponents/edit.blade.php
+++ resources/views/intervieweeAttributesComponents/edit.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Edit Interviewee</title>
 </head>
+
 <body>
     <form method="POST" action="{{ route('intervieweeAttributes.update',$interviewee->id) }}">
         @csrf
@@ -13,29 +15,30 @@
         <input type="text" id="name" class="@error('interviewee_types_id') is-invalid @enderror " name="name" value="{{$interviewee->name}}"><br>
         @error('name')
         <div class="alert alert-danger error-login">{{ $message }}</div>
-    @enderror
+        @enderror
         <select name="interviewee_types_id" class="@error('interviewee_types_id') is-invalid @enderror " id="interviewee_types_id">
-            @foreach ($intervieweesT as $i)   
-                 @if($i->id===$interviewee->interviewee_types_id)
+            @foreach ($intervieweesT as $i)
+            @if($i->id===$interviewee->interviewee_types_id)
             <option value="{{$i->id}}">{{$i->name}}</option>
-             @break
-                 @endif
-           @endforeach
-        @foreach ($intervieweesT as $i) 
-        @if($i->id!==$interviewee->interviewee_types_id)
-        <option value="{{$i->id}}">{{$i->name}}</option>
+            @break
+            @endif
+            @endforeach
+            @foreach ($intervieweesT as $i)
+            @if($i->id!==$interviewee->interviewee_types_id)
+            <option value="{{$i->id}}">{{$i->name}}</option>
 
-             @endif
-    
-        @endforeach
-        
+            @endif
+
+            @endforeach
+
         </select>
         @error('interviewee_types_id')
         <div class="alert alert-danger error-login">{{ $message }}</div>
-    @enderror
+        @enderror
         <button type="submit" class="btn btn-primary mt-2">Edit</button>
-      </form> 
-      
-    
+    </form>
+
+
 </body>
+
 </html>
--- resources/views/interviewerComponents/table.blade.php
+++ resources/views/interviewerComponents/table.blade.php
@@ -1,7 +1,7 @@
 @extends("layouts.layout")
 <style>
-    .alert{
-        color:red;
+    .alert {
+        color: red;
     }
 </style>
 @section('content')
@@ -56,40 +56,41 @@
                     {{ $i -> role }} <!-- Qet Emrin e Userit -->
                 </th>
                 <td class="py-4 px-6 text-blue-600">
-                    <a href="{{route('interviewer.edit',$i->id)}}">Edit</a> <!-- Edit Button Here -->
+                    <!-- Edit Button Here -->
+                    <button type="button" data-modal-toggle="editUserModal" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</button>
                 </td>
-             
+
                 <td class="py-4 px-6 text-red-600">
                     <!--<a href="{{ route('interviewer.destroy', $i -> id) }}">Delete</a>  Delete Button Here -->
-    
-                    <button  type="button" data-modal-toggle="deleteUserModal{{$i->id}}" class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</button>
+
+                    <button type="button" data-modal-toggle="deleteUserModal{{$i->id}}" class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</button>
                 </td>
             </tr>
             <!-- Delete Modal -->
-<div id="deleteUserModal{{$i->id}}" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
-    <div class="relative p-4 w-full max-w-md h-full md:h-auto">
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="deleteUserModal{{$i->id}}">
-                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                </svg>
-                <span class="sr-only">Close modal</span>
-            </button>
-            <div class="p-6 text-center">
-                <svg aria-hidden="true" class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                </svg>
-                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this Interviewee Types?</h3>
-                <a href="{{ route('interviewer.destroy', $i -> id) }}">
-                    <button data-modal-toggle="deleteUserModal{{$i->id}}" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                        Yes, I'm sure
-                    </button>
-                </a>
-                <button data-modal-toggle="deleteUserModal{{$i->id}}" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600" data-modal-toggle="deleteUserModal{{$i->id}}">No, cancel</button>
+            <div id="deleteUserModal{{$i->id}}" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
+                <div class="relative p-4 w-full max-w-md h-full md:h-auto">
+                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                        <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="deleteUserModal{{$i->id}}">
+                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                            </svg>
+                            <span class="sr-only">Close modal</span>
+                        </button>
+                        <div class="p-6 text-center">
+                            <svg aria-hidden="true" class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                            </svg>
+                            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this Interviewee Types?</h3>
+                            <a href="{{ route('interviewer.destroy', $i -> id) }}">
+                                <button data-modal-toggle="deleteUserModal{{$i->id}}" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                                    Yes, I'm sure
+                                </button>
+                            </a>
+                            <button data-modal-toggle="deleteUserModal{{$i->id}}" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600" data-modal-toggle="deleteUserModal{{$i->id}}">No, cancel</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</div>
             @endforeach
         </tbody>
     </table>
@@ -105,7 +106,7 @@
             <!-- Modal header -->
             <div class="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
                 <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                    Create Interviewer 
+                    Create Interviewer
                 </h3>
                 <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="addUserModal">
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -121,28 +122,28 @@
                         <input type="text" name="name" id="name" autocomplete="given-name" class="@error('name') is-invalid @enderror bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Name" required>
                         @error('name')
                         <div class="alert alert-danger error-login">{{ $message }}</div>
-                    @enderror
+                        @enderror
                     </div>
                     <div>
                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
                         <input type="email" name="email" id="email" autocomplete="given-name" class="@error('email') is-invalid @enderror bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Email" required>
                         @error('email')
                         <div class="alert alert-danger error-login">{{ $message }}</div>
-                    @enderror
+                        @enderror
                     </div>
                     <div>
                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Password</label>
                         <input type="password" name="password" id="password" autocomplete="given-name" class="@error('password') is-invalid @enderror bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Password" required>
                         @error('password')
                         <div class="alert alert-danger error-login">{{ $message }}</div>
-                    @enderror
+                        @enderror
                     </div>
                     <div>
                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Role</label>
                         <input type="text" name="role" id="role" autocomplete="given-name" class="@error('role') is-invalid @enderror bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Role" required>
                         @error('role')
                         <div class="alert alert-danger error-login">{{ $message }}</div>
-                    @enderror
+                        @enderror
                     </div>
                 </div>
             </div>
@@ -158,14 +159,14 @@
 <div id="editUserModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
     <div class="relative p-4 w-full max-w-md h-full md:h-auto">
         <!-- Modal content -->
-        <form method="POST" action="{{ route('interviewer.store') }}" class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <form method="POST" action="{{route('interviewer.edit',$i->id)}}" class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             @csrf
             <!-- Modal header -->
             <div class="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
                 <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                    Create Interviewer 
+                    Create Interviewer
                 </h3>
-                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="addUserModal">
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="editUserModal">
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                     </svg>
@@ -179,34 +180,34 @@
                         <input type="text" name="name" id="name" autocomplete="given-name" class="@error('name') is-invalid @enderror bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Name" required>
                         @error('name')
                         <div class="alert alert-danger error-login">{{ $message }}</div>
-                    @enderror
+                        @enderror
                     </div>
                     <div>
                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
                         <input type="email" name="email" id="email" autocomplete="given-name" class="@error('email') is-invalid @enderror bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Email" required>
                         @error('email')
                         <div class="alert alert-danger error-login">{{ $message }}</div>
-                    @enderror
+                        @enderror
                     </div>
                     <div>
                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Password</label>
                         <input type="password" name="password" id="password" autocomplete="given-name" class="@error('password') is-invalid @enderror bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Password" required>
                         @error('password')
                         <div class="alert alert-danger error-login">{{ $message }}</div>
-                    @enderror
+                        @enderror
                     </div>
                     <div>
                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Role</label>
                         <input type="text" name="role" id="role" autocomplete="given-name" class="@error('role') is-invalid @enderror bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Role" required>
                         @error('role')
                         <div class="alert alert-danger error-login">{{ $message }}</div>
-                    @enderror
+                        @enderror
                     </div>
                 </div>
             </div>
             <!-- Modal footer -->
             <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
-                <button type="submit" data-modal-toggle="addUserModal" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create</button>
+                <button type="submit" data-modal-toggle="editUserModal" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create</button>
             </div>
         </form>
     </div>
