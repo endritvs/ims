decb700e06d261c418bc140492350f4cd396947f
endritvs <104865231+endritvs@users.noreply.github.com>
2022-09-06 13:23:12 +0200

    Revert "new-up"

--- resources/resources/views/includes/head.blade.php
+++
@@ -1,5 +0,0 @@
-<meta charset="utf-8">
-<meta name="description" content="">
-<meta name="Saquib" content="Blade">
-<title>Checkout our layout</title>
-<link rel="stylesheet" href="/css/app.css">
--- resources/resources/views/pages/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-@extends("layouts.layout")
-
-@section('content')
-
-{{-- @if(Auth::user()->role==="interviewee")
-
-<h1>interviewee</h1>
-@endif --}}
-@endsection
