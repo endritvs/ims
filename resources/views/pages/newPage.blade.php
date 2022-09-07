86c0adc6e7ec430f4584e557907de12ff181b656
Albert <ialbertbislimi@gmail.com>
2022-09-07 14:42:01 -0700

    Updated
    1.) Updated Timeline

--- resources/views/pages/newPage.blade.php
+++ resources/views/pages/newPage.blade.php
@@ -237,21 +237,21 @@
               }
             </style>
             
-            <ol class="items-center h-[500px] text-center sm:flex no-scrollbar overflow-x-auto">
+            <ol class="h-[500px] text-center sm:flex no-scrollbar overflow-x-auto h-auto">
               @foreach($interview as $i)
-              <li class="mx-[10px] relative mb-6 sm:mb-0 min-w-[350px]">
-                  <div class="flex items-center ">
+              <li class="mx-[10px] relative mb-6 sm:mb-0 min-w-[350px] {{$i->id%2==0 ? '' : 'mt-[23.8rem]'}}">
+                @if($i->id%2!=0)
+                  <div class="flex items-center mb-5">
                       <div class="flex z-10 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
                           <svg aria-hidden="true" class="w-3 h-3 text-blue-600 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
                       </div>
                       <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
                   </div>
+                @endif
                   
-                  <!-- ========== Card => Start ============ -->
-                  
-                  {{-- <div class="mt-3 p-[10px] sm:pr-8 border-2 rounded-[10px]  bg-slate-600 border-solid"> --}}
-                    
-                      {{-- <h3 class="text-lg font-semibold text-gray-900 dark:text-white">{{$i -> user -> name}}</h3> --}}
+                  <!-- ==================================== -->
+                  <!-- ========== Card => Start =========== -->
+                  <!-- ==================================== -->
                       {{-- <h3 class="text-lg font-semibold text-gray-900 dark:text-white">{{$i -> interviewees -> name}}</h3>
                       <p class="text-base font-normal text-white dark:text-white">{{$i->interviewees->interviewee_type->name}}</p>
                       <p class="text-base font-normal text-white dark:text-white">@foreach ($i->interviewees->interviewee_type->interviewee_attributes as $s)
@@ -346,8 +346,17 @@
                             </div>
                           
                           </div>
-                        </div>   
-              </li>
+                        </div>
+                        
+                      @if($i->id%2==0)
+                        <div class="flex items-center mt-5">
+                            <div class="flex z-10 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+                                <svg aria-hidden="true" class="w-3 h-3 text-blue-600 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                            </div>
+                            <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+                        </div>
+                      @endif 
+                      </li>
 
               @endforeach
             </ol>
