ba194f741827ed8f7083f68f358bb0d4cb419fa9
Albert <ialbertbislimi@gmail.com>
2022-09-16 14:09:43 +0200

    Review Card
    Finished Review Card

--- resources/views/components/reviewCard.blade.php
+++ resources/views/components/reviewCard.blade.php
@@ -1,21 +1,144 @@
 <div class="h-full ml-14 mt-10 mb-10 md:ml-64">
     <div class="w-full">
-      <section class=" mx-auto px-4 sm:px-6 lg:px-4 py-12">
-        <div class="relative flex flex-col justify-center overflow-hidden py-6 sm:py-12">
+      <section class="px-4 sm:px-6 lg:px-4 py-12">
+        <div class="relative justify-center overflow-hidden py-6 sm:py-12">
             <div class="text-center">
                 <h1 class="text-[25px] font-medium">We've found you some perfect Candidates</h1>
                 <p class="mb-10 text-[15px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum suscipit</p>
             </div>
-                <div class="mx-auto flex flex-col rounded-3xl bg-gray-900 p-7">
-                    <div class="flex flex-col items-center rounded-lg md:flex-row ">
-                        <img src="img/me-about.jpg" alt="profile" class="w-[150px] h-[200px] rounded-lg ">
-                        <div class="flex flex-col justify-between p-4 leading-normal">
-                            <h2 class="mb-2 text-2xl font-bold tracking-tight">Albert</h2>
-                            <p class="mb-3 font-normal">Since beginning my journey as a self-taught developer, I've been coding and creating freshly websites for clients around the world.</br> I have a diverse set of skills, ranging from design, to HTML + CSS + Javascript, all the way to React.</p>
+                <div class="flex relative rounded-3xl bg-blue-900 mx-10 p-7">
+                    <div class="flex-1">
+                        <div class="flex">
+                            <img src="img/me-about.jpg" alt="profile" class="w-[150px] h-[200px] rounded-lg">
+                            <h2 class="text-white ml-[35px] text-[25px] mt-[70px] font-[500]">Albert</h2>
                         </div>
-                        <div class="flex flex-col">
-                            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm mb-5 px-5 py-2.5  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Work with "name"</button>
-                            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Remove from list</button>
+                        <div class="mt-5">
+                            <p class="font-normal w-[65%] text-white">Since the beginning of my journey as a self-taught developer, I've been coding and creating freshly websites for clients around the world.</p>
+                        </div>
+                        <div class="pt-3">
+                            <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">HTML</span>
+                            <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">React</span>
+                            <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">Javascript</span>
+                        </div>
+                        <div class="flex items-center pt-5">
+                            <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                            <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                            <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                            <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                            <svg aria-hidden="true" class="w-5 h-5 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                            <p class="ml-2 text-sm font-medium text-white dark:text-gray-400">4.95 out of 5</p>
+                        </div>
+                    </div>
+                    <div class="flex-1">
+                        
+                        {{-- RATING SECTION --}}
+                        <div class="gap-8 sm:grid">
+                            <div>
+                                <dl>
+                                    <dt class="text-sm font-medium text-white dark:text-gray-400">Staff</dt>
+                                    <dd class="flex items-center mb-3">
+                                        <div class="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 mr-2">
+                                            <div class="bg-blue-600 h-2.5 rounded dark:bg-blue-500" style="width: 88%"></div>
+                                        </div>
+                                        <span class="text-sm font-medium text-white dark:text-gray-400">8.8</span>
+                                    </dd>
+                                </dl>
+                                <dl>
+                                    <dt class="text-sm font-medium text-white dark:text-gray-400">Comfort</dt>
+                                    <dd class="flex items-center mb-3">
+                                        <div class="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 mr-2">
+                                            <div class="bg-blue-600 h-2.5 rounded dark:bg-blue-500" style="width: 89%"></div>
+                                        </div>
+                                        <span class="text-sm font-medium text-white dark:text-gray-400">8.9</span>
+                                    </dd>
+                                </dl>
+                                <dl>
+                                    <dt class="text-sm font-medium text-white dark:text-gray-400">Free WiFi</dt>
+                                    <dd class="flex items-center mb-3">
+                                        <div class="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 mr-2">
+                                            <div class="bg-blue-600 h-2.5 rounded dark:bg-blue-500" style="width: 88%"></div>
+                                        </div>
+                                        <span class="text-sm font-medium text-white dark:text-gray-400">8.8</span>
+                                    </dd>
+                                </dl>
+                                <dl>
+                                    <dt class="text-sm font-medium text-white dark:text-gray-400">Facilities</dt>
+                                    <dd class="flex items-center">
+                                        <div class="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 mr-2">
+                                            <div class="bg-blue-600 h-2.5 rounded dark:bg-blue-500" style="width: 54%"></div>
+                                        </div>
+                                        <span class="text-sm font-medium text-white dark:text-gray-400">5.4</span>
+                                    </dd>
+                                </dl>
+                            </div>
+                        </div>
+                        {{-- RATING SECTION END --}}
+                            <div class="h-auto w-[400px] pt-10 rounded-[12px]">
+                                <p class="text-xl font-semibold text-white cursor-pointer transition-all">Add Comment/Questions</p>
+                                <textarea class="h-auto px-3 text-sm py-1 mt-5 outline-none border-pink-300 w-full resize-none border rounded-lg placeholder:text-sm" rows="5" placeholder="Add your comments here"></textarea>  
+                                
+                                <div class="flex justify-between mt-2"> 
+                                    <button class="h-12 w-[150px] bg-blue-400 text-sm text-white rounded-lg transition-all cursor-pointer hover:bg-blue-600">Submit comment</button>
+                                    <p class="text-sm text-blue-900 ">Enter atleast 15 characters</p>
+                                </div>
+                            </div>  
+                        
+                    </div>
+
+                    {{-- <div class="flex flex-row-2 items-center rounded-lg md:flex-row ">
+                        <div class="text-white justify-between p-4 leading-normal">
+                            <img src="img/me-about.jpg" alt="profile" class="w-[150px] h-[200px] rounded-lg float-left">
+                            <div class="flex text-center">
+                            <h2 class="text-2xl font-bold pl-[15px] pt-[70px] tracking-tight">Albert</h2>
+                            </div>
+                            <div class="float-left">
+                            
+                            <p class="mb-3 w-[200px] font-normal">Since the beginning of my journey as a self-taught developer, I've been coding and creating freshly websites for clients around the world.</p>
+                            </div>
+                            <div class="flex gap-4">
+                            <div class="text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-green-200 text-green-700 rounded-full">
+                                <i class="fa-brands fa-react"> React JSX</i>
+                                
+                            </div>
+                            <div class="text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-green-200 text-green-700 rounded-full">
+                                <i class="fa-brands fa-react"> HTML</i>
+                               
+                            </div>
+                            <div class="text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-green-200 text-green-700 rounded-full">
+                                <i class="fa-brands fa-react"> CSS</i>
+                                
+                            </div>
+                        </div>
+                        </div>
+                        <div class="flex flex-col pl-[60px]">
+                            <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm mb-5 px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Work with "name"</button>
+                            <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm mb-5 px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Remove from list</button>
+                            <button class="block w-full md:w-auto text-white bg-gradient-to-r from-cyan-500 to-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="small-modal">Add Comment</button>
+                        </div>
+                        <div id="small-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
+                            <div class="relative p-4 w-full max-w-md h-full md:h-auto">
+                                <!-- Modal content -->
+                                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                    <!-- Modal header -->
+                                    <div class="flex justify-between items-center p-5 rounded-t border-b dark:border-gray-600">
+                                        <h3 class="text-xl font-medium text-gray-900 dark:text-white">
+                                            Add Comments/Questions
+                                        </h3>
+                                        <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="small-modal">
+                                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                            <span class="sr-only">Close modal</span>
+                                        </button>
+                                    </div>
+                                    <!-- Modal body -->
+                                    <div class="h-80 px-7 w-full rounded-[12px] bg-transparent-100 p-4">
+                                        <textarea class="h-auto px-3 text-sm py-1 mt-5 outline-none border-pink-300 w-full resize-none border rounded-lg placeholder:text-sm" placeholder="Add your comments here"></textarea>  
+                                        
+                                        <div class="flex justify-between mt-2"> <button class="h-12 w-[150px] bg-blue-400 text-sm text-white rounded-lg transition-all cursor-pointer hover:bg-blue-600">Submit comment</button>
+                                            <p class="text-sm text-blue-900 ">Enter atleast 15 characters</p>
+                                        </div>   
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="flex items-center mt-4">
@@ -24,8 +147,9 @@
                         <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                         <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                         <svg aria-hidden="true" class="w-5 h-5 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                        <p class="ml-2 text-sm font-medium text-gray-500 dark:text-gray-400">4.95 out of 5</p>
-                    </div>
+                        <p class="ml-2 text-sm font-medium text-white dark:text-gray-400">4.95 out of 5</p>
+                    </div> --}}
+                        
                 </div>
         </div>
       </section>
