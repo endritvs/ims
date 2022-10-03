955ec8a049c7388b3a5291a7ff9be594c96c8376
Albert <ialbertbislimi@gmail.com>
2022-10-03 14:43:11 +0200

    changes in dashboard, profile, head, and user

--- resources/views/components/user.blade.php
+++ resources/views/components/user.blade.php
@@ -1,83 +1,127 @@
 <div class="fixed w-full flex items-center h-14 text-white z-10">
+    <div class="flex w-full justify-end h-14 bg-blue-900 dark:bg-gray-900 header-right">
+        <!-- Profile dropdown -->
+        <div class="flex items-center md:order-2 mr-3">
+            <button type="button"
+                class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
+                data-dropdown-placement="bottom">
+                <span class="sr-only">Open user menu</span>
+                @php
+                    $link = explode('/', Auth::user()->img);
+                @endphp
+                @if (Auth::user()->img === 'public/noProfilePhoto/nofoto.jpg')
+                    <div
+                        class="inline-flex overflow-hidden relative justify-center items-center w-10 h-10 bg-gray-100 rounded-full dark:bg-gray-600">
+                        <span class="font-medium text-gray-600 dark:text-white">
+                            <img src="{{ asset('/noProfilePhoto/' . $link[2]) }}">
+                        </span>
 
-  <div class="flex w-full justify-end h-14 bg-blue-900 dark:bg-gray-900 header-right">
-    
-    <!-- Profile dropdown -->
-    <div class="relative flex">
-      <div class="flex items-center pr-2">
-        <button type="button" class="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-          <span class="sr-only">Open user menu</span>
-          <img class="h-10 w-10 rounded-full object-cover" src="/img/me-about.jpg" alt="">
-        </button>
-      </div>
+                    </div>
+                    <span class="font-medium text-white relative bottom-3 capitalize">{{ Auth::user()->name }}</span>
+                @else
+                    <div
+                        class="inline-flex overflow-hidden relative justify-center items-center w-10 h-10 bg-gray-100 rounded-full dark:bg-gray-600">
+                        <span class="font-medium text-gray-600 dark:text-white">
+                            <img src="/storage/img/{{ $link[2] }}">
+                        </span>
 
+                    </div>
+                @endif
+            </button>
+            <!-- Dropdown menu -->
+            <div class="hidden z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
+                id="user-dropdown">
+                <div class="py-3 px-4">
+                    <span
+                        class="block text-sm text-gray-900 dark:text-white">{{ Auth::user()->name . ' ' . Auth::user()->lastname }}</span>
+                    <span
+                        class="block text-sm font-medium text-gray-900 truncate dark:text-white">{{ Auth::user()->email }}</span>
+                </div>
+                <ul class="py-1" aria-labelledby="user-menu-button">
+                    <li>
+                        <a href="{{ route('interview.editProfile', Auth::user()->id) }}"
+                            class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-500 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                            <i class="fa-solid fa-user mr-1"></i>
+                            Profile
+                        </a>
+                    </li>
+                    <form
+                        class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-500 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                        method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <li>
+                            <button type="submit">
+                                <span class="inline-flex mr-1">
+                                    <i class="fa-solid fa-right-from-bracket"></i>
+                                </span>
+                                Logout
+                            </button>
+                        </li>
+                    </form>
+                </ul>
+            </div>
+        </div>
 
+        <ul class="flex items-center">
+            <li>
+                <button id="theme-toggle" type="button"
+                    class="text-white dark:text-white hover:bg-gray-300 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1">
+                    <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                    </svg>
+                    <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                            fill-rule="evenodd" clip-rule="evenodd"></path>
+                    </svg>
+                </button>
+            </li>
+            <li>
+                <div class="block w-px h-6 mx-3 bg-gray-400 dark:bg-gray-700 ml-1"></div>
+            </li>
+        </ul>
     </div>
 
-    <ul class="flex items-center">
-      <li>
-        <button id="theme-toggle" type="button" class="text-white dark:text-white hover:bg-gray-300 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
-          <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
-          <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-      </button>
-      </li>
-
-      <li>
-        <div class="block w-px h-6 mx-3 bg-gray-400 dark:bg-gray-700"></div>
-      </li>
-      <form class="" method="POST" action="{{ route('logout') }}">
-        @csrf
-        <li>
-          <button type="submit" class="flex items-center mr-4 hover:text-blue-100">
-            <span class="inline-flex mr-1">
-              <i class="fa-solid fa-right-from-bracket"></i>
-            </span>
-            Logout
-          </button>
-        </li>
-      </form>
-    </ul>
-  </div>
-  <script>
-    var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-    var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-
-    // Change the icons inside the button based on previous settings
-    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        themeToggleLightIcon.classList.remove('hidden');
-    } else {
-        themeToggleDarkIcon.classList.remove('hidden');
-    }
-
-    var themeToggleBtn = document.getElementById('theme-toggle');
-
-    themeToggleBtn.addEventListener('click', function() {
-
-    // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle('hidden');
-    themeToggleLightIcon.classList.toggle('hidden');
-
-    // if set via local storage previously
-    if (localStorage.getItem('color-theme')) {
-        if (localStorage.getItem('color-theme') === 'light') {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
+     {{-- DARK MODE BUTTON  --}}
+    <script>
+        var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+        var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+        // Change the icons inside the button based on previous settings
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia(
+                '(prefers-color-scheme: dark)').matches)) {
+            themeToggleLightIcon.classList.remove('hidden');
         } else {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
+            themeToggleDarkIcon.classList.remove('hidden');
         }
+        var themeToggleBtn = document.getElementById('theme-toggle');
+        themeToggleBtn.addEventListener('click', function() {
 
-    // if NOT set via local storage previously
-    } else {
-        if (document.documentElement.classList.contains('dark')) {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        } else {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        }
-    }
-    
-});
-  </script>
+            // toggle icons inside button
+            themeToggleDarkIcon.classList.toggle('hidden');
+            themeToggleLightIcon.classList.toggle('hidden');
+
+            // if set via local storage previously
+            if (localStorage.getItem('color-theme')) {
+                if (localStorage.getItem('color-theme') === 'light') {
+                    document.documentElement.classList.add('dark');
+                    localStorage.setItem('color-theme', 'dark');
+                } else {
+                    document.documentElement.classList.remove('dark');
+                    localStorage.setItem('color-theme', 'light');
+                }
+                // if NOT set via local storage previously
+            } else {
+                if (document.documentElement.classList.contains('dark')) {
+                    document.documentElement.classList.remove('dark');
+                    localStorage.setItem('color-theme', 'light');
+                } else {
+                    document.documentElement.classList.add('dark');
+                    localStorage.setItem('color-theme', 'dark');
+                }
+            }
+        });
+    </script>
 </div>
--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -6,13 +6,6 @@
         <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1">
 
             <div tabindex="0" aria-label="form" class="focus:outline-none w-full bg-white p-10 dark:bg-gray-800">
-                <div class="md:flex items-center border-b pb-6 border-gray-200">
-                    <div class="flex items-center md:mt-0 mt-4">
-
-
-                    </div>
-
-                </div>
                 <h1 tabindex="0" role="heading" aria-label="profile information"
                     class="focus:outline-none text-3xl font-bold text-gray-800 mt-12 dark:text-white">Profile info</h1>
                 <p tabindex="0"
@@ -24,29 +17,30 @@
                 <p tabindex="0"
                     class="focus:outline-none text-sm font-light leading-none text-black mt-0.5 dark:text-white">Your
                     details and place of birth</p>
-                <form method="POST" enctype="multipart/form-data" action="{{ route('interview.updateProfile', Auth::user()->id) }}">
+                <form method="POST" enctype="multipart/form-data"
+                    action="{{ route('interview.updateProfile', Auth::user()->id) }}">
                     @csrf
                     <div class="mt-8 md:flex items-center">
                         <div class="flex flex-col">
                             <label class="mb-3 text-sm leading-none text-black dark:text-white">Name</label>
                             <input type="name" name="name" tabindex="0"
-                                class=" @error('name') is-invalid @enderror focus:outline-none focus:ring-2 focus:ring-indigo-400 w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"
+                                class=" @error('name') is-invalid @enderror focus:outline-none focus:ring-2 focus:ring-indigo-400 w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-2 border rounded border-gray-200"
                                 value="{{ Auth::user()->name }}" />
-                               
-                                @error('name')
-                            <div class="ml-1 text-red-500 text-xs alert alert-danger">{{ $message }}</div>
-                        @enderror
+
+                            @error('name')
+                                <div class="ml-1 text-red-500 text-xs alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class="flex flex-col md:ml-12 md:mt-0 mt-8">
                             <label class="mb-3 text-sm leading-none text-gray-800 dark:text-white">Email</label>
                             <input type="email" name="email" tabindex="0"
-                                class="@error('email') is-invalid @enderror focus:outline-none focus:ring-2 focus:ring-indigo-400 w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"
+                                class="@error('email') is-invalid @enderror focus:outline-none focus:ring-2 focus:ring-indigo-400 w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-2 border rounded border-gray-200"
                                 value="{{ Auth::user()->email }}" />
-                                @error('email')
+                            @error('email')
                                 <div class="ml-1 text-red-500 text-xs alert alert-danger">{{ $message }}</div>
                             @enderror
                         </div>
-                        
+
                     </div>
                     @php
                         
@@ -57,10 +51,10 @@
                         <div class="flex flex-col">
                             <label class="mb-3 text-sm leading-none text-gray-800 dark:text-white">Image</label>
                             <input type="file" name="img" tabindex="0"
-                            class="@error('img') is-invalid @enderror focus:outline-none focus:ring-2 focus:ring-indigo-400 w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-1 border rounded border-gray-200"/>
+                                class="@error('img') is-invalid @enderror focus:outline-none focus:ring-2 focus:ring-indigo-400 w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 border rounded border-gray-200" />
                             @error('img')
-                            <div class="ml-1 text-red-500 text-xs alert alert-danger">{{ $message }}</div>
-                        @enderror
+                                <div class="ml-1 text-red-500 text-xs alert alert-danger">{{ $message }}</div>
+                            @enderror
 
                             @if (Auth::user()->img === 'public/noProfilePhoto/nofoto.jpg')
                                 <img class="mt-3 rounded object-cover" src="{{ asset('/noProfilePhoto/' . $link[2]) }}" width="50px"
@@ -72,32 +66,24 @@
                         </div>
                         <div class="flex flex-col md:ml-11 md:mt-[-60px]">
                             <label class="mb-3 text-sm leading-none text-gray-800 dark:text-white">Company Name</label>
-                            <input value="{{Auth::user()->company->company_name}}" type="text" name="company_name" tabindex="0"
-                            class="@error('company_name') is-invalid @enderror focus:outline-none focus:ring-2 focus:ring-indigo-400 w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-3 border rounded border-gray-200"/>
+                            <input value="{{ Auth::user()->company->company_name }}" type="text" name="company_name"
+                                tabindex="0"
+                                class="@error('company_name') is-invalid @enderror focus:outline-none focus:ring-2 focus:ring-indigo-400 w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-2 border rounded border-gray-200" />
                             @error('company_name')
-                            <div class="ml-1 text-red-500 text-xs alert alert-danger">{{ $message }}</div>
-                        @enderror
-
-                         
+                                <div class="ml-1 text-red-500 text-xs alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
-                        
                     </div>
-
                     <button role="button" aria-label="Next step"
-                        class="flex items-center justify-center py-2 px-4 focus:outline-none bg-blue-700 hover:bg-blue-800  border rounded border-gray-400 mt-5 md:mt-12 hover:bg-gray-100  focus:ring-2 focus:ring-offset-2 focus:ring-gray-700 hover:bg-gray-400">
+                        class="flex items-center justify-center py-2 px-4 focus:outline-none bg-blue-900 border rounded border-gray-400 mt-7 md:mt-14 hover:bg-blue-800  focus:ring-2 focus:ring-offset-2 focus:ring-gray-700">
                         <span class="text-sm font-medium text-center text-white capitalize">Save</span>
-
                     </button>
-
             </div>
             </form>
             <div tabindex="0" aria-label="form" class="focus:outline-none w-full bg-white p-10 dark:bg-gray-800">
                 <div class="md:flex items-center border-b pb-6 border-gray-200">
                     <div class="flex items-center md:mt-0 mt-4">
-
-
                     </div>
-
                 </div>
                 <h1 tabindex="0" role="heading" aria-label="profile information"
                     class="focus:outline-none text-3xl font-bold text-gray-800 mt-12 dark:text-white">Change Password</h1>
@@ -110,10 +96,10 @@
                 <p tabindex="0"
                     class="focus:outline-none text-sm font-light leading-none text-gray-600 mt-0.5 dark:text-white">Your
                     details and place of birth</p>
-                
-                    <form method="POST" enctype="multipart/form-data" action="{{ route('interview.updatePassword') }}">
-                        @csrf
-                        @if (session('status'))
+
+                <form method="POST" enctype="multipart/form-data" action="{{ route('interview.updatePassword') }}">
+                    @csrf
+                    @if (session('status'))
                         <div class="mt-3 ml-1 text-green-700 text-md alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
@@ -123,48 +109,42 @@
                         </div>
                     @endif
                     <div class="mt-8 md:flex items-center">
-                    <div class="flex flex-col">
-                      
-                 
-                        <label class="mb-3 text-sm leading-none text-black dark:text-white">Current-Password</label>
-                        <input placeholder="Current-Password" type="password" tabindex="0"
-                            aria-label="Enter current password" name="old_password"
-                            class="@error('old_password') is-invalid @enderror focus:outline-none focus:ring-2 focus:ring-indigo-400 w-64 bg-gray-100 text-sm font-medium leading-none text-black p-3 border rounded border-gray-200" />
+                        <div class="flex flex-col">
+                            <label class="mb-3 text-sm leading-none text-gray-800 dark:text-white">Current Password</label>
+                            <input placeholder="Current-Password" type="password" tabindex="0"
+                                aria-label="Enter current password" name="old_password"
+                                class="@error('old_password') is-invalid @enderror focus:outline-none focus:ring-2 focus:ring-indigo-400 w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-2 border rounded border-gray-200" />
                             @error('old_password')
-                            <span class="ml-1 text-red-500 text-xs alert alert-dangerml-1 text-red-500 text-xs alert alert-danger">{{ $message }}</span>
-                        @enderror
+                                <span
+                                    class="ml-1 text-red-500 text-xs alert alert-dangerml-1 text-red-500 text-xs alert alert-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
                     </div>
-                    
-                </div>
-                <div class="mt-12 md:flex items-center justify-start">
-                    <div class="flex flex-col md:mb-12 md:mt-0 mt-8">
-                        <label class="mb-3 text-sm leading-none text-gray-800 dark:text-white">New-Password</label>
-                        <input placeholder="New-Password" type="password" tabindex="0" name="new_password"
-                            class="@error('new_password') is-invalid @enderror focus:outline-none focus:ring-2 focus:ring-indigo-400 w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-3 border rounded border-gray-200" />
+                    <div class="mt-12 md:flex items-center justify-start">
+                        <div class="flex flex-col md:mb-12 md:mt-0 mt-8">
+                            <label class="mb-3 text-sm leading-none text-gray-800 dark:text-white">New Password</label>
+                            <input placeholder="New-Password" type="password" tabindex="0" name="new_password"
+                                class="@error('new_password') is-invalid @enderror focus:outline-none focus:ring-2 focus:ring-indigo-400 w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-2 border rounded border-gray-200" />
                             @error('new_password')
-                            <span class="ml-1 text-red-500 text-xs alert alert-danger">{{ $message }}</span>
-                        @enderror
-                    </div>
+                                <span class="ml-1 text-red-500 text-xs alert alert-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
 
-                    <div class="flex flex-col md:mb-12 md:mt-0 mt-8 ml-4">
-                        <label class="mb-3 text-sm leading-none text-gray-800 dark:text-white">Confirm-New-Password</label>
-                        <input placeholder="Confirm New Password" type="password" tabindex="0" name="new_password_confirmation"
-                            aria-label="Confirm New Password"
-                            class="@error('new_password_confirmation') is-invalid @enderror focus:outline-none focus:ring-2 focus:ring-indigo-400 w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-3 border rounded border-gray-200" />
+                        <div class="flex flex-col md:mb-12 md:mt-0 mt-8 ml-4">
+                            <label class="mb-3 text-sm leading-none text-gray-800 dark:text-white">Confirm Password</label>
+                            <input placeholder="Confirm New Password" type="password" tabindex="0"
+                                name="new_password_confirmation" aria-label="Confirm New Password"
+                                class="@error('new_password_confirmation') is-invalid @enderror focus:outline-none focus:ring-2 focus:ring-indigo-400 w-64 bg-gray-100 text-sm font-medium leading-none text-gray-800 p-2 border rounded border-gray-200" />
                             @error('new_password_confirmation')
-                            <span class="ml-1 text-red-500 text-xs alert alert-danger">{{ $message }}</span>
-                        @enderror
-                     </div>
-
-                </div>
-
-
-                <button role="button" aria-label="Next step"
-                    class="flex items-center justify-center py-2 px-4 focus:outline-none bg-blue-700 hover:bg-blue-800 border rounded border-gray-400 mt-7 md:mt-0 hover:bg-gray-100  focus:ring-2 focus:ring-offset-2 focus:ring-gray-700 hover:bg-gray-400">
-                    <span class="text-sm font-medium text-center text-white capitalize">Save</span>
-
-                </button>
-            </form>
+                                <span class="ml-1 text-red-500 text-xs alert alert-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                    </div>
+                    <button role="button" aria-label="Next step"
+                        class="flex items-center justify-center py-2 px-4 focus:outline-none bg-blue-900 border rounded border-gray-400 mt-2 md:mt-14 hover:bg-blue-800 focus:ring-2 focus:ring-offset-2 focus:ring-gray-700 ">
+                        <span class="text-sm font-medium text-center text-white capitalize">Save</span>
+                    </button>
+                </form>
             </div>
         </div>
     </div>
